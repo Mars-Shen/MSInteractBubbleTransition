9b9c240c565df08ca940130bd58889333de5db2b
Mars Shen <look1239@qq.com>
2016-07-12 21:11:53 -0500

    change file structure

--- Demo/SecondViewController.swift
+++ Demo/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: UIViewController{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.backgroundColor = UIColor.blueColor()
+        self.view.backgroundColor = UIColor.redColor()
 
     }
 
