cbb54c563bc6e5cdd56a4da0539a2163a1089e29
Mars Shen <look1239@gmail.com>
2016-07-15 23:27:00 -0500

    remove Pods/ directory

--- Example/Pods/Target Support Files/MSInteractBubbleTransition/MSInteractBubbleTransition-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MSInteractBubbleTransition : NSObject
-@end
-@implementation PodsDummy_MSInteractBubbleTransition
-@end
--- Example/Pods/Target Support Files/Pods-MSInteractBubbleTransition_Example/Pods-MSInteractBubbleTransition_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_MSInteractBubbleTransition_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MSInteractBubbleTransition_ExampleVersionString[];
-
